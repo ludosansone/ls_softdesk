2522b6ec2eade44fac8617ac368c35b62d369321
Ludovic SANSONE <libreaudio63@gmail.com>
2023-01-11 08:42:31 +0100

    Ajout du modèle Project

--- softdesk/api/models.py
+++ softdesk/api/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+class Project(models.Model):
+    title = models.CharField(
+                             verbose_name='titre',
+                             max_length=200,
+                             null=False)
+    description = models.TextField(
+                                   max_length=2000,
+                                   null=False)
+    type = models.CharField(
+                            max_length=100,
+                            null=False)
+    author = models.ForeignKey(
+                               get_user_model(),
+                               null=False,
+                               on_delete=models.CASCADE)
