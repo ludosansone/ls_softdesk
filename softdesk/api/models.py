0a19d8c1c1e0e99a4303c1491a4fdff5ca451253
Ludovic SANSONE <libreaudio63@gmail.com>
2023-01-11 09:01:36 +0100

    Ajout du modèle Issue

--- softdesk/api/models.py
+++ softdesk/api/models.py
@@ -17,3 +17,45 @@ class Project(models.Model):
                                get_user_model(),
                                null=False,
                                on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+
+class Issue(models.Model):
+    title = models.CharField(
+                             verbose_name='titre',
+                             max_length=200,
+                             null=False)
+    description = models.TextField(
+                                   max_length=2000,
+                                   null=False)
+    tag = models.CharField(
+                           max_length=100,
+                           null=False)
+    priority = models.CharField(
+                                max_length=100,
+                                null=False)
+    status = models.CharField(
+                              max_length=100,
+                              null=False)
+    created_time = models.DateTimeField(
+                                        verbose_name='date et heure de création',
+                                        auto_now_add=True)
+    project = models.ForeignKey(
+                                Project,
+                                on_delete=models.CASCADE,
+                                null=False)
+    author = models.ForeignKey(
+                               get_user_model(),
+                               on_delete=models.CASCADE,
+                               null=False,
+                               related_name='author')
+    assigned = models.ForeignKey(
+                                get_user_model(),
+                                on_delete=models.CASCADE,
+                                null=False,
+                                related_name='assigned')
+
+    def __str__(self):
+        return self.title
