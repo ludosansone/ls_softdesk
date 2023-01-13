480781336772be72bd3c78b2caaa95a63242ba71
Ludovic SANSONE <libreaudio63@gmail.com>
2023-01-13 14:21:32 +0100

    Ajout du serializer ProjectDetailSerializer

--- softdesk/api/serializers.py
+++ softdesk/api/serializers.py
@@ -6,3 +6,9 @@ class ProjectListSerializer(ModelSerializer):
     class Meta:
         model = Project
         fields = ('id', 'title', 'description')
+
+
+class ProjectDetailSerializer(ModelSerializer):
+    class Meta:
+        model = Project
+        fields = ('id', 'title', 'description', 'type', 'author', 'contributors')
--- softdesk/api/views.py
+++ softdesk/api/views.py
@@ -1,11 +1,17 @@
 from rest_framework.viewsets import ModelViewSet
-from api.serializers import ProjectListSerializer
+from api.serializers import ProjectListSerializer, ProjectDetailSerializer
 from api.models import Project
 
 
 class ProjectViewSet(ModelViewSet):
-    serializer_class = ProjectListSerializer
+    list_serializer = ProjectListSerializer
+    detail_serializer = ProjectDetailSerializer
 
     def get_queryset(self):
         queryset = Project.objects.all()
         return queryset
+
+    def get_serializer_class(self):
+        if self.action == 'retrieve':
+            return self.detail_serializer
+        return self.list_serializer
