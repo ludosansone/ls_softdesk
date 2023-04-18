28657ba3743ef9474669b6b500f3cf2345644d02
Ludovic SANSONE <libreaudio63@gmail.com>
2023-04-18 13:45:58 +0200

    Ajout des permissions

---
+++ softdesk/api/permissions/contributor.py
@@ -0,0 +1,17 @@
+from rest_framework.permissions import BasePermission
+
+class IsContributor(BasePermission):
+    def has_permission(self, request, view):
+        return True
+
+    def has_object_permission(self, request, view, obj):
+        if obj.author == request.user:
+            return True
+
+        contributors = obj.contributors.all()
+
+        for contributor in contributors:
+            if contributor.user == request.user:
+                return True
+                
+        return False
